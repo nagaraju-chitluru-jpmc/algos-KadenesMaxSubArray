420c934d9a3659c496b96b841db004cb3eb65403
Nagaraju, Chitluru <cn_raju@hotmail.com>
2022-10-22 12:56:04 +0800

    Added README.md

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,6 @@ public class Main {
         System.out.println("Max sub-array sum is " + maxSubArraySum(inputData));
     }
 
-
-    // https://www.youtube.com/watch?v=2MmGzdiKR9Y
-    // If sum of previous array elements is less than current element then we can discard previous array and
-    // start summing from current element.
-    // If previous sum is not less than current one, then keep summing. While doing so, the previous sum might be
-    // reduced due to current element being negative. For this, we maintain global sum to keep track of max sum value.
     static int maxSubArraySum(int[] inputData) {
         if(inputData.length == 0)
             throw new ArrayIndexOutOfBoundsException("input data should contain at least one element ");
